7158707d1a661bad56af25a782c90ba5739a31c2
Ajay Vishnu V <52136562+ajay-vishnu@users.noreply.github.com>
2021-02-08 19:58:16 +0530

    Update main.js

--- Udemy/js/main.js
+++ Udemy/js/main.js
@@ -1,13 +1,8 @@
-var numbers = [3, 8, 7, 9, 6, 1, 12, 5, 14, 2, 19, 16, 18, 11, 10, 15, 13, 17, 20];
+var btn = document.getElementById('search-button');
 
-var numbers_sorted = numbers.sort(function(a, b){return a - b;});
-
-var first_num = numbers_sorted[0];
-
-for (let i = 0; i < numbers_sorted.length; i++) {
-    if (first_num != numbers_sorted[i]) {
-        console.log("The missing element is " + first_num + ".");
-        break;
-    }
-    first_num++;
+function buttonClicked()    {
+    var txt = document.getElementById('search-bar').value;
+    console.log("User searched for " + txt + ".");
 }
+
+btn.addEventListener('click', buttonClicked);
